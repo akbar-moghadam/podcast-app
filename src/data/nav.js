9e01fb43cb2897b81ade4a96d6488278e53dd521
akbar-moghadam <moghadam4@gmail.com>
2021-04-01 22:08:15 +0430

    add nav

---
+++ src/data/nav.js
@@ -0,0 +1,9 @@
+export const Nav=[
+    {to:"همه اپیزودها",link:"/all"},
+    {to:"وبلاگ بی پلاس",link:"/weblog"},
+    {to:"از کجا بخریم",link:"/weherbuy"},
+    {to:"پشتیبانی",link:"/backup"},
+    {to:"خبرنامه",link:"/Newsletters"},
+    {to:"درباره ما",link:"/abaut"}
+
+] 
---
+++ src/page/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../data/nav';
+
+function navigation() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default navigation
