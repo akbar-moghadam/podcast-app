9fd0f8508720cbaebe2a7891cf72382ec69bdc5c
akbar-moghadam <moghadam4@gmail.com>
2021-04-02 02:41:25 +0430

    add navigation to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import navigation from './page/navigation';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-     
+    <div className="nav">{navigation}</div>
       <Switch>
             <Route exact path="/"><Home /></Route>
             <Route path="/all"> <All /> </Route>
