e8c61a27b64e067b5a8f743f5bcfc9836c5391bb
akbar-moghadam <moghadam4@gmail.com>
2021-04-06 00:18:29 +0430

    full nav and add home

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import Navigation from './navigation';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../css/home.scss'
 
 function Home() {
     return (
-        <div>
-          <Navigation/>  
+        <div className="main-container">
+           <div className="buy">
+           <button type="button"><Link to="/">مشاهده و خرید</Link></button>
+              <h1>مرچندایزپادکست</h1> 
+             
+            </div> 
         </div>
     )
 }
